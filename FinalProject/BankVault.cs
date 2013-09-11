dffd1775c297445751244b001c33c61b8b086122
miguelgallegos <miguel.gallegos@gmail.com>
2013-09-10 22:23:48 -0700

    backVault class body added

--- FinalProject/BankVault.cs
+++ FinalProject/BankVault.cs
@@ -10,5 +10,41 @@ namespace FinalProject
     class BankVault
     {
 
+        private decimal bankBalance;
+        private object vaultLock;
+
+        public BankVault(decimal bankBalance)
+        {
+            this.bankBalance = bankBalance;
+            this.vaultLock = new object();
+        }
+
+        public decimal Deposit(decimal amount)
+        {
+            lock (vaultLock) {
+                bankBalance += amount;
+                return bankBalance;
+            }
+            
+        }
+        
+        //stops sim if false
+        public bool Withdraw(decimal amount, decimal v)
+        {
+            lock (vaultLock) {
+
+                if (amount <= bankBalance)
+                {
+                    bankBalance -= amount;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
+
     }
 }
